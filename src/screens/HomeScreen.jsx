e2e8671f28818e88231ac93d72b09786ccd63282
Ana Beatriz <ana.github0@gmail.com>
2023-05-03 16:53:01 -0300

    tirando o botão quiz da home

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -33,17 +33,7 @@ export default function HomeScreen({ navigation }) {
       >
         Redefinir Senha
       </Button>
-
-      <Button
-        style={styles.button}
-        mode="contained"
-        onPress={() => {
-          navigation.navigate("GameScreen");
-        }}
-      >
-        Quiz
-      </Button>
-      
+  
     </View>
   );
 }
