557157200ad780ad56ede3c01557953477f5f7e4
Ana Beatriz <ana.github0@gmail.com>
2023-05-03 13:47:33 -0300

    imagem está aparecendo conforme a pregunta aparece

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -12,26 +12,31 @@ export default function GameScreen({ navigation }) {
       pergunta: "Nezuko morre?",
       resposta1: "Não",
       resposta2: "Sim",
+      img: "nezuco-morre.jpg",
     },
     {
       pergunta: "Quantos Onis Tanjiro matou?",
       resposta1: 4,
       resposta2: 10,
+      img: "tanjiro.png",
     },
     {
       pergunta: "Qual o objetivo de Demon Slayer?",
       resposta1: "Achar a cura para salvar a Nezuko",
       resposta2: "Matar Onis",
+      img: "objetivodemon-slayer-3.jpg",
     },
     {
       pergunta: "Qual o nome do corvo de Tanjiro?",
       resposta1: "Ytagarasu",
       resposta2: "Jiummi",
+      img: "corvotanjiro4.png",
     },
     {
       pergunta: "Tanjiro usa a respiração do sol?",
       resposta1: "Sim",
       resposta2: "Não",
+      img: "respiracao-do-sol-tanjiro.jpg",
     },
   ];
 
@@ -40,18 +45,36 @@ export default function GameScreen({ navigation }) {
 
     return (
       <>
-        <Text style={styles.text}>{currentQuestion.pergunta}</Text>
-        <Button mode="contained" style={styles.button}>
+        <Image
+          source={require(`../img/${currentQuestion.img}`)}
+          style={{ width: 250, height: 150 }}
+        />
+
+        <Text style={styles.text}>
+          {currentQuestion.pergunta}
+        </Text>
+
+        <Button 
+          mode="contained" 
+          style={styles.button}
+        >
           {currentQuestion.resposta1}
         </Button>
-        <Button mode="contained" style={styles.button}>
+
+        <Button 
+          mode="contained" 
+          style={styles.button}
+        >
           {currentQuestion.resposta2}
         </Button>
+
         <Button
           mode="contained"
           style={styles.button}
           onPress={() =>
-            setCurrentQuestionIndex((currentQuestionIndex + 1) % questions.length)
+            setCurrentQuestionIndex(
+              (currentQuestionIndex + 1) % questions.length
+            )
           }
         >
           Mudar Pergunta
@@ -60,13 +83,5 @@ export default function GameScreen({ navigation }) {
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <Image
-        source={require("../img/tanjiro.png")}
-        style={{ width: 250, height: 150 }}
-      />
-      {aparecerAsPerguntas()}
-    </View>
-  );
+  return <View style={styles.container}>{aparecerAsPerguntas()}</View>;
 }
